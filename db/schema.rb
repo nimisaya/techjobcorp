1a8d7a4b289ee6d5f1ed31a8aae14abd37db257a
LucySE <lucywirth@gmail.com>
2021-01-14 12:03:53 +1100

    added profile pic string to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_123633) do
+ActiveRecord::Schema.define(version: 2021_01_14_005941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_123633) do
     t.text "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile_picture"
   end
 
 end
