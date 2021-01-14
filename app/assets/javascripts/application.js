dc333c0f87b03fd5b41861936f01887c1a11c56a
Brooke <brooke_vaneerden@hotmail.com>
2021-01-15 10:36:23 +1100

    salary logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,10 @@
 
 
 
-//========= VARIABLES ========//
+//========= GAMES.JS - WIll be moved to its own .js========//
+// initial output from JSON which is transfromed into an array
+let output = ""
+
 // array of questions
 let my_json = ""
 
@@ -52,40 +55,6 @@ function put_score(){
   );
 }
 
-<<<<<<< HEAD
-
-//-------------------CALCULATE SALARY FUNCTION-------------------//
-// Calculate users salary based on their score and time
-function calculateSalary(){
-
-  const k = 1000;
-  const timeBonus = 40 * k;
-
-  //1.  To get full bonus you must complete each question in half a minute
-  const fastestRequiredTime = total_questions * 30;
-
-  //2.  This is a made up time penalty based on every minute of the fastest time
-  let timePenalty = (total_time - fastestRequiredTime) * 1000
-
-  //3.  The time penalty will never be a negative number
-  if (timePenalty >= timeBonus) {
-    timePenalty = timeBonus;
-  }
-
-  //4.  Income (total salary) is calculated
-  let income = current_score / total_questions * (100 * k) + timeBonus - timePenalty;
-
-  //5. Lowest allowable income is $30,000.00 (minimum salary)
-  if (income <= 30000){
-    salary = 30000
-  }
-  else {
-    salary = income
-  }
-
-} // End calculateSalary
-
-=======
 // function delayed_load(){
 //   setTimeout(
 //     function()
@@ -94,59 +63,41 @@ function calculateSalary(){
 //    }, 1000);
 //
 // } - seems to cause questions not to load to begin with
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
 
 
 
 $(document).ready(function () {
 
-  // actions function to retreive api data
-    getPuzzles()
+// actions function to retreive api data
+  getPuzzles()
 
 
-<<<<<<< HEAD
-  // checks is jquery is working
-    console.log($)
+// checks is jquery is working
+  console.log($)
 
-
-  //-----------------GET PUZZLE APIS-------------------//
-  //retrives APIs from http://localhost:3000/api/puzzles/:id
-   function getPuzzles(){
-=======
 //retrives APIs from http://localhost:3000/api/puzzles/:id
  function getPuzzles(){
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-    //1. outputs the API request and convers to an array (my_json)
-    $.getJSON('/api/puzzles/'+game_id).done((data)=>{
-      console.log(data)
-      my_json=data
+  //game_id is retreived from the HTML
+    output = $.getJSON('/api/puzzles/'+game_id)
 
-    //2. appends puzzle data to HTML when API data has been loaded
-    update_question()
-
-    //3. determines the total number of questions - sets variable
-    total_questions=data.questions.length
-
-    })
-
-  }; // end of getPuzzles
+  //outputs the API request and convers to an array (my_json)
+  $.getJSON('/api/puzzles/'+game_id).done((data)=>{
+    console.log(data)
+    my_json=data
 
 
-<<<<<<< HEAD
+  //appends puzzle data to HTML when API data has been loaded
+  update_question()
 
+  //determines the total number of questions - sets variable
+  total_questions=data.questions.length
 
-  //----------------SHUFFLE MULTIPLE CHOICE ANSWERS-------------------//
-  // Function shuffles multiple choice answers (Array of answers)
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
+  })
 
-=======
+}; // end of getPuzzles
+
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -156,101 +107,65 @@ function shuffleArray(array) {
 
 // this function updates the Questions & Score when next/back is clicked
 function update_question(){
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-  //-----------------UPDATE QUESTIONS-------------------//
-  // this function updates the Questions & Score when next/back is clicked
-  function update_question(){
+  // question is pulled using [current_question] as an index
+  const check_question = my_json.questions[current_question]
 
-<<<<<<< HEAD
-    // question is pulled using [current_question] as an index
-    const check_question = my_json.questions[current_question]
-=======
   if (current_question === total_questions-1){
     console.log("END QUESTION")
     // $('#buttons').html("<button type='button' class = 'move_button' id ='finish' >Finish Quiz</button>") --- no longer needed
     $("#next").css({display : 'none'})
     $("#finish").css({opacity :1})
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-    // When final question is reached the finish button appears
-    if (current_question === total_questions-1){
-      // $('#buttons').html("<button type='button' class = 'move_button' id ='finish' >Finish Quiz</button>") --- no longer needed
-      $("#next").css({display : 'none'})
-      $("#finish").css({opacity :1})
-    }
+  }
 
-    // Set the question
-    $('#question').html(check_question.question)
+  // Set the question
+  $('#question').html(check_question.question)
 
-    // Set the options
-    let optionA = "<label>" +
-    "<input type='radio' id='incorrect' name='option'>" +
-    "<span class='label-body' id='incorrect_a'>"+ check_question.incorrect_a + "</span></label>";
+  // Set the options
+  let optionA = "<label>" +
+  "<input type='radio' id='incorrect' name='option'>" +
+  "<span class='label-body' id='incorrect_a'>"+ check_question.incorrect_a + "</span></label>";
 
-    let optionB = "<label>" +
-    "<input type='radio' id='incorrect' name='option'>" +
-    "<span class='label-body' id='incorrect_b'>"+ check_question.incorrect_b + "</span></label>";
+  let optionB = "<label>" +
+  "<input type='radio' id='incorrect' name='option'>" +
+  "<span class='label-body' id='incorrect_b'>"+ check_question.incorrect_b + "</span></label>";
 
-    let optionC = "<label>" +
-    "<input type='radio' id='incorrect' name='option'>" +
-    "<span class='label-body' id='incorrect_c'>"+ check_question.incorrect_c + "</span></label>";
+  let optionC = "<label>" +
+  "<input type='radio' id='incorrect' name='option'>" +
+  "<span class='label-body' id='incorrect_c'>"+ check_question.incorrect_c + "</span></label>";
 
-    let answer = "<label>" +
-    "<input type='radio' id='correct' name='option'>" +
-    "<span class='label-body' id='solution'>"+ check_question.solution + "</span></label>";
+  let answer = "<label>" +
+  "<input type='radio' id='correct' name='option'>" +
+  "<span class='label-body' id='solution'>"+ check_question.solution + "</span></label>";
 
-    //set array of answers
-    const answersArray = [optionA, optionB, optionC, answer];
+  const answersArray = [optionA, optionB, optionC, answer];
 
-    //shuffle answers
-    shuffleArray(answersArray);
+  shuffleArray(answersArray);
 
-<<<<<<< HEAD
-    // Appends multiple choice answers to the HTML (after shuffling)
-    $('#optionA').html(answersArray[0]);
-    $('#optionB').html(answersArray[1]);
-    $('#optionC').html(answersArray[2]);
-    $('#optionD').html(answersArray[3]);
-
-    //Updates the current score on the HTML
-    $('#current_score').text("Points: " + current_score)
-=======
   $('#optionA').html(answersArray[0]);
   $('#optionB').html(answersArray[1]);
   $('#optionC').html(answersArray[2]);
   $('#optionD').html(answersArray[3]);
 
   $('#current_score').text("Points: " + current_score)
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-    //this stores previous question data to be used for feedback
-    previous_question = check_question.question
-    correct_answer = check_question.solution
+  //this stores previous question data to be used for feedback
+  previous_question = check_question.question
+  correct_answer = check_question.solution
 
 
-    // This will be used to animate the expression of the interviewer depending on whether the previous answer was true/false
+  // This will be used to animate the expression of the interviewer depending on whether the previous answer was true/false
 
-    // IF statement that changes the interviewers face depending on the previous answer
-    if(current_question != 0){
-      if(correct === true){
-        $('#animation').attr("src","https://piskel-imgstore-b.appspot.com/img/83b24d6b-5627-11eb-88f0-2f8693b953d1.gif")
-        $('#answer').text('Your previous answer was correct!')
-      } else {
-        $('#animation').attr("src","https://piskel-imgstore-b.appspot.com/img/26014fe6-5628-11eb-9b50-2f8693b953d1.gif")
-        $('#answer').text('Your previous answer was false. The answer to the question ' + previous_question + ' was '+ correct_answer)
-      }
+  // IF statement that changes the interviewers face depending on the previous answer
+  if(current_question != 0){
+    if(correct === true){
+      $('#animation').attr("src","https://piskel-imgstore-b.appspot.com/img/83b24d6b-5627-11eb-88f0-2f8693b953d1.gif")
+      $('#answer').text('Your previous answer was correct!')
+    } else {
+      $('#animation').attr("src","https://piskel-imgstore-b.appspot.com/img/26014fe6-5628-11eb-9b50-2f8693b953d1.gif")
+      $('#answer').text('Your previous answer was false. The answer to the question ' + previous_question + ' was '+ correct_answer)
     }
-<<<<<<< HEAD
-  } //end update_question function
-
-
-
-  //-----------------NEXT BUTTON-------------------//
-  // next button
-  $("#next").click(function() {
-    console.log('NEXT')
-=======
   }
 }
 
@@ -259,28 +174,22 @@ function update_question(){
 // next button
 $("#next").click(function() {
   console.log('NEXT')
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-  // checks if current page is less than maximum page (last question)
-    if(current_question <= total_questions){
+// checks if current page is less than maximum page (last question)
+  if(current_question <= total_questions){
 
-  //function checks which radio button has been clicked - adds score
-      check_radio()
+//function checks which radio button has been clicked - adds score
+    check_radio()
 
-  //adds +1 to current question
-      current_question++
+//adds +1 to current question
+    current_question++
 
-  //runs function to update shown question & score
-      update_question()
-      console.log(current_question)
+//runs function to update shown question & score
+    update_question()
+    console.log(current_question)
 
-<<<<<<< HEAD
-    }
-  }) //end next button function
-=======
   }
 })
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
 // //back button
 // $("#back" ).click(function() {
@@ -297,91 +206,61 @@ $("#next").click(function() {
 // })
 
 
-<<<<<<< HEAD
-  //-----------------CHECK RADIO BUTTONS-------------------//
-  // SCORE CHECK FUNCTION  //
-  function check_radio() {
-=======
 // SCORE CHECK FUNCTION - FOR MANDAA //
 function check_radio() {
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-  // 1. checks if correct radio button has been checked
-     if($('#correct').is(':checked')) {
+// 1. checks if correct radio button has been checked
+   if($('#correct').is(':checked')) {
 
-  // 2. if correct button has been checked add +100 to scores
-    current_score = current_score+100
+// 2. if correct button has been checked add +100 to scores
+  current_score = current_score+100
 
-  // changes "correct" variable to true. This will be used to animate a gif if previous answeer is correct or false
-    correct = true
-    console.log(current_score)
-    }
-
-  //3. if incorrect button has been clicked minus 30 from the score
-    else{
-      current_score = current_score-30
-        console.log(current_score)
-        correct = false
-    }
+// changes "correct" variable to true. This will be used to animate a gif if previous answeer is correct or false
+  correct = true
+  console.log(current_score)
   }
 
-<<<<<<< HEAD
+//3. if incorrect button has been clicked minus 30 from the score
+  else{
+    current_score = current_score-30
+      console.log(current_score)
+      correct = false
+  }
+}
 
-  //-----------------TIMER-------------------//
-  window.onload = setInterval(function(){
-    total_time = total_time + 1
-=======
 // on screen timer
 // THIS CODE IS A MESS - need to clean
 window.onload = setInterval(function(){
 
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
 
-    // if seconds = 59 add to minute variable
-    if (second_timer === 59){
-      minute_timer = minute_timer+1
-      //resent seconds timer
-      second_timer = 0
-      $('#timer').text("This is a timer - could use?: "+minute_timer+ " : "+second_timer)
+  // if seconds = 59 add to minute variable
+  if (second_timer === 59){
+    minute_timer = minute_timer+1
+    //resent seconds timer
+    second_timer = 0
+    $('#timer').text("This is a timer - could use?: "+minute_timer+ " : "+second_timer)
 
-    }
-    if (second_timer <= 10){
+  }
+  if (second_timer <= 10){
+  second_timer = second_timer + 1
+  $('#timer').text("This is a timer - could use?: "+minute_timer+ " : 0"+second_timer)
+}
+
+  if (second_timer >= 10){
     second_timer = second_timer + 1
-    $('#timer').text("This is a timer - could use?: "+minute_timer+ " : 0"+second_timer)
+    $('#timer').text("This is a timer - could use?: "+minute_timer+ " : "+second_timer)
   }
 
-<<<<<<< HEAD
-    if (second_timer >= 10){
-      second_timer = second_timer + 1
-      $('#timer').text("This is a timer - could use?: "+minute_timer+ " : "+second_timer)
-    }
-
-  }, 1000) //end onscreen timer
-
-
-  //-----------------START TIMER-------------------//
-  window.onload = setInterval(function(){
-  $("#finish").click(function() {
-    calculateSalary()
-    put_score()
-  })
-  }, 10)
-
-
-
-}) // END document.ready
-=======
 }, 1000)
 
 window.onload = setInterval(function(){
 $("#finish").click(function() {
   put_score()
 })
-}, 10)
+}, 1000)
 })
 
 
 
 // REECE'S WIP getting post working (anyone can continue this! <3)
 // $.post( "test.php", { score: current_score} )
->>>>>>> 0b892ed43f3d551b58ab89033a5cf21589641c65
